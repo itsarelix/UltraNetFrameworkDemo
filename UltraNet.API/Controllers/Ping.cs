b763b6afd8708477a592ec1241e14217fd2cef60
itsarelix <itsarelix@gmail.com>
2025-07-22 07:48:58 -0400

    add ping

---
+++ UltraNet.API/Controllers/Ping.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UltraNet.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PingController : ControllerBase
+    {
+
+
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("API is alive!");
+        }
+
+
+
+    }
+
+}
+
+
